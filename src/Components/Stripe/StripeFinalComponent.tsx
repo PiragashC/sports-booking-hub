5b6e9c9d507edd4e44a4a5e61f843ff8c0615083
Piragash <pragashconstantine13@gmail.com>
2025-02-04 12:25:28 +0530

    Add Stripe integration for payment processing and update dependencies

---
+++ src/Components/Stripe/StripeFinalComponent.tsx
@@ -0,0 +1,46 @@
+import { loadStripe, Stripe } from '@stripe/stripe-js';
+import React, { useEffect, useState } from 'react'
+import StripePayment from './StripePayment';
+import apiRequest from '../../Utils/apiRequest';
+
+const StripeFinalComponent = () => {
+    const [stripePromise, setStripePromise] = useState<Promise<Stripe | null> | null>(null);
+    const [clientSecret, setClientSecret] = useState<string>("");
+
+    useEffect(() => {
+        apiRequest<{ publishableKey: string }>({ method: "get", url: "/config" })
+            .then((r) => {
+                console.log(r.publishableKey, "dfvvdcxx");
+                setStripePromise(loadStripe(r.publishableKey));
+            })
+            .catch((error) => {
+                console.error("Error loading Stripe.js library:", error);
+                setStripePromise(null);
+            });
+    }, []);
+
+    useEffect(() => {
+        apiRequest({
+            method: "post",
+            url: "/create-payment-intent",
+            data: {}
+        })
+            .then((result: any) => {
+                console.log(result.clientSecret, "Client Secret fetched");
+                setClientSecret(result.clientSecret || "");
+            })
+            .catch((error) => {
+                console.error("Error fetching client secret:", error);
+                setClientSecret('');
+            });
+    }, []);
+
+
+    return (
+        <>
+            {stripePromise && clientSecret && <StripePayment stripePromise={stripePromise} clientSecret={clientSecret} />}
+        </>
+    )
+}
+
+export default StripeFinalComponent
